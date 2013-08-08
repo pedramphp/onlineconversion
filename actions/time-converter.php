8d0f114a0632237d26342e60d76206177d7be053
Farzin Fadamin <ffadamin@gmail.com>
2013-08-09 00:34:13 +0430

    Fix area action

--- actions/time-converter.php
+++ actions/time-converter.php
@@ -7,8 +7,8 @@
 			LiteFrame::IncludeJson();
 			LiteFrame::SetTemplateAction("global-converter");
 			
-			$site = new Site("AreaPage");
-			LiteFrame::$yAction["SiteData"]["activePage"] = "areaPage";
+			$site = new Site("TimePage");
+			LiteFrame::$yAction["SiteData"]["activePage"] = "timePage";
 		}
 		
 	}
